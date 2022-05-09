5485f4281734854c486d9d3bcb8d5b6c6907130e
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-05-09 16:41:49 +0300

    Using Unity Events v1

--- Assets/PixelCrew/HeroInputReader.cs
+++ Assets/PixelCrew/HeroInputReader.cs
@@ -1,24 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HeroInputReader : MonoBehaviour
 {
     [SerializeField] private Hero _hero;
 
-    public void Update()
+    public void OnHorizontalMovement(InputAction.CallbackContext context)
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        _hero.SetDirection(horizontal);
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    _hero.SetDirection(-1);
-        //}
-        //else if (Input.GetKey(KeyCode.D))
-        //{
-        //    _hero.SetDirection(1);
-        //}
-        //else
-        //{
-        //    _hero.SetDirection(0);
-        //}
+        var direction = context.ReadValue<float>();
+        _hero.SetDirection(direction);
     }
 }
