2dfeb5e07b1dbc68b08dff547cae71bca7bd7619
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-05-10 18:19:28 +0300

    Physics and Collisions

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -3,19 +3,47 @@
 public class Hero : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpSpeed;
+
+    [SerializeField] private LayerCheck _groundCheck;
+
     private Vector2 _direction;
+    private Rigidbody2D _rigidbody;
 
     public void SetDirection(Vector2 direction)
     {
         _direction = direction;
     }
 
-    public void Update()
+    public void Awake()
     {
-        if (_direction.magnitude > 0)
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+        bool isJumping = _direction.y > 0;
+        if (isJumping)
         {
-            Vector2 delta = _direction * _speed * Time.deltaTime;
-            transform.position += new Vector3(delta.x, delta.y, transform.position.z);
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+        } else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
         }
+    }
+    private bool IsGrounded()
+    {
+        return _groundCheck.IsTouchingLayer;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = IsGrounded() ? Color.green : Color.red;
+        Gizmos.DrawSphere(transform.position + new Vector3(-0.5f, 0.5f, 0), 0.1f);
     }
 }
