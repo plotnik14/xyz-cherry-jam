1a81be0260f5faa5c90ee9ff7bab2e0953a73569
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-05-13 14:52:04 +0300

    Fixed double jump

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -27,7 +27,7 @@ public class Hero : MonoBehaviour
         bool isJumping = _direction.y > 0;
         if (isJumping)
         {
-            if (IsGrounded())
+            if (IsGrounded() && _rigidbody.velocity.y <= 0)
             {
                 _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
             }
