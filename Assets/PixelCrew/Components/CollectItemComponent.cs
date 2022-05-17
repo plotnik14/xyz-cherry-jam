f6d3d44241afc1cbf5f9ab749ec2d02aab4e500e
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-05-17 11:11:43 +0300

    HW4 - Tilemaps and camera

---
+++ Assets/PixelCrew/Components/CollectItemComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class CollectItemComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _objectToCollect;
+        [SerializeField] private HeroInventory _inventory;
+
+        private const int SilverCoinValue = 1;
+        private const int GoldCoinValue = 10;
+
+        public void CollectCoin()
+        {
+            if (_objectToCollect.CompareTag("SilverCoin"))
+            {
+                _inventory.AddCoins(SilverCoinValue);
+            }
+            else if (_objectToCollect.CompareTag("GoldCoin"))
+            {
+                _inventory.AddCoins(GoldCoinValue);
+            }
+            else
+            {
+                Debug.LogError($"Untagged coin:{gameObject.name}");
+            }
+        }
+    }
+
+}
---
+++ Assets/PixelCrew/HeroInventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace PixelCrew
+{
+    public class HeroInventory : MonoBehaviour
+    {
+        [SerializeField] private int _coins;
+
+        public void Awake()
+        {
+            _coins = 0;
+        }
+
+        public void AddCoins(int coinsCount)
+        {
+            _coins += coinsCount;
+            PrintCoinsCount();
+        }
+
+        private void PrintCoinsCount()
+        {
+            Debug.Log($"Coins count:{_coins}");
+        }
+    }
+}
+
