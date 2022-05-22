c0d8f31d696b82eff5142485690b74b5a7cba16d
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-05-22 17:19:14 +0300

    Added Unit test example + added assemblies

---
+++ Assets/Tests/SimpleModelTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using PixelCrew;
+
+namespace Editor
+{
+    internal class SimpleModelTest
+    {
+        private SimpleModel _model;
+
+        [SetUp]
+        public void Init()
+        {
+            _model = new SimpleModel(2, 3);
+        }
+
+        [Test]
+        public void TestIncreaseValue()
+        {
+            _model.Increase();
+            Assert.AreEqual(2, _model.Value);
+        }
+
+        [Test]
+        public void TestDecreaseValue()
+        {
+            _model.Decrease();
+            Assert.AreEqual(-3, _model.Value);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // release resources 
+        }
+    }
+}
