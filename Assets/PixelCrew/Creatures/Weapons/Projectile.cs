a96b6e3fc85363d8e4789331321db38fa6b9948a
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-07-19 21:24:34 +0300

    Projectile for seashell

--- Assets/PixelCrew/Creatures/Weapons/Projectile.cs
+++ Assets/PixelCrew/Creatures/Weapons/Projectile.cs
@@ -5,15 +5,18 @@ namespace PixelCrew.Creatures.Weapons
     public class Projectile : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private bool _invertX;
 
         private Rigidbody2D _rigidbody;
         private int _direction;
+
 
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
 
-            _direction = transform.lossyScale.x > 0 ? 1 : -1;
+            var mod = _invertX ? -1 : 1;
+            _direction = transform.lossyScale.x * mod > 0 ? 1 : -1;
             var force = new Vector2(_direction * _speed, 0);
             _rigidbody.AddForce(force, ForceMode2D.Impulse);
         }
