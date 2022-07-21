9e14f11c10c02f151e730627fcbb4d8344713cd0
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-07-21 14:21:39 +0300

    Seashell Part 2

--- Assets/PixelCrew/Creatures/Mobs/ShootingTrapAI.cs
+++ Assets/PixelCrew/Creatures/Mobs/ShootingTrapAI.cs
@@ -1,13 +1,75 @@
 ﻿using PixelCrew.Utils;
 using PixelCrew.Components;
 using UnityEngine;
+using System;
 
 namespace PixelCrew.Creatures
 {
     public class ShootingTrapAI : MonoBehaviour
     {
+        [SerializeField] private LayerCheck _vision;
+
+        [Header("Melee")]
+        [SerializeField] private Cooldown _meleeCooldown;
         [SerializeField] private CheckCircleOverlap _meleeAttack;
+        [SerializeField] private LayerCheck _meleeCanAttack;
+
+        [Header("Range")]
+        [SerializeField] private Cooldown _rangeCooldown;
+        [SerializeField] private SpawnComponent _rangeAttack;
+
+        private Animator _animator;
+
+        protected static readonly int Melee = Animator.StringToHash("melee");
+        protected static readonly int Range = Animator.StringToHash("range");
+        
 
 
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (_vision.IsTouchingLayer)
+            {
+                if (_meleeCanAttack.IsTouchingLayer)
+                {
+                    if (_meleeCooldown.IsReady)
+                    {
+                        MeleeAttack();
+                    }
+                    return;
+                }
+
+                if (_rangeCooldown.IsReady)
+                {
+                    RangeAttack();
+                }
+            }
+        }
+
+        private void RangeAttack()
+        {
+            _rangeCooldown.Reset();
+            _animator.SetTrigger(Range);
+        }
+
+        private void MeleeAttack()
+        {
+            _meleeCooldown.Reset();
+            _animator.SetTrigger(Melee);
+        }
+
+        public void OnMeleeAttack()
+        {
+            _meleeAttack.Check();
+        }
+
+        public void OnRangeAttack()
+        {
+            _rangeAttack.Spawn();
+        }
     }
 }
