2f2bbb1660f31c7223e59ea52015c8a8ce9593bd
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-07-27 15:03:42 +0300

    Echo Effect

---
+++ Assets/PixelCrew/Components/Audio/SoundEffectZoneComponent.cs
@@ -0,0 +1,40 @@
+﻿using PixelCrew.Utils;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace PixelCrew.Components
+{
+    public class SoundEffectZoneComponent : MonoBehaviour
+    {
+        [SerializeField] private string _tag;
+        [SerializeField] private LayerMask _layer = ~0;
+
+        [SerializeField] private AudioMixerSnapshot _main;
+        [SerializeField] private AudioMixerSnapshot _withEcho;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!Check(other)) return;
+
+            _withEcho.TransitionTo(0.1f);
+            Debug.Log("Transition to Echo");
+        }
+
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (!Check(other)) return;
+
+            _main.TransitionTo(0.1f);
+            Debug.Log("Transition to Main");
+        }
+
+        private bool Check(Collider2D other)
+        {
+            if (!other.gameObject.IsInLayer(_layer)) return false;
+            if (!string.IsNullOrEmpty(_tag) && !other.gameObject.CompareTag(_tag)) return false;
+
+            return true;
+        }
+    }
+}
