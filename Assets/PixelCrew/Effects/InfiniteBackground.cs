70ae14ccba986d253d33aeb2d00d4cc612173c25
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-08-25 14:32:24 +0300

    Refactoring for InfiniteBackground

--- Assets/PixelCrew/Effects/InfiniteBackground.cs
+++ Assets/PixelCrew/Effects/InfiniteBackground.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using PixelCrew.Utils;
+﻿using PixelCrew.Utils;
 using UnityEngine;
 
 namespace PixelCrew.Effects
@@ -11,8 +10,7 @@ namespace PixelCrew.Effects
 
         private Bounds _containerBounds;
         private Bounds _allBounds;
-
-        private Vector3 _boundsToTransformDelta;
+        
         private Vector3 _containerDelta;
         private Vector3 _screenSize;
 
@@ -25,19 +23,17 @@ namespace PixelCrew.Effects
             }
 
             _allBounds = _containerBounds;
-
-            _boundsToTransformDelta = transform.position - _allBounds.center;
-            _containerDelta = _container.position - _allBounds.center;
+            _containerDelta = _container.position - _containerBounds.center;
         }
 
         private void LateUpdate()
         {
-            var min = _camera.ViewportToWorldPoint(Vector3.zero); // left and bottom
-            var max = _camera.ViewportToWorldPoint(Vector3.one); // right and top
+            var min = _camera.ViewportToWorldPoint(Vector3.zero); // bottom left
+            var max = _camera.ViewportToWorldPoint(Vector3.one); // top right
 
             _screenSize = new Vector3(max.x - min.x, max.y - min.y);
-            _allBounds.center = transform.position - _boundsToTransformDelta;
             var camPosition = _camera.transform.position.x;
+            
             var screenLeft = new Vector3(camPosition - _screenSize.x / 2, _containerBounds.center.y);
             var screenRight = new Vector3(camPosition + _screenSize.x / 2, _containerBounds.center.y);
 
@@ -48,7 +44,7 @@ namespace PixelCrew.Effects
             
             if (!_allBounds.Contains(screenRight))
             {
-                InstantiateContainer(_allBounds.max.x + _containerBounds.extents.x); // extent - половина размера bound ?
+                InstantiateContainer(_allBounds.max.x + _containerBounds.extents.x);
             }
         }
 
@@ -57,15 +53,16 @@ namespace PixelCrew.Effects
             var newBounds = new Bounds(new Vector3(boundCenterX, _containerBounds.center.y), _containerBounds.size);
             _allBounds.Encapsulate(newBounds);
 
-            _boundsToTransformDelta = transform.position - _allBounds.center;
             var newContainerXPos = boundCenterX + _containerDelta.x;
             var newPosition = new Vector3(newContainerXPos, _container.transform.position.y);
             Instantiate(_container, newPosition, Quaternion.identity, transform);
         }
 
-        private void OnDrawGizmosSelected()
+        // private void OnDrawGizmosSelected()
+        private void OnDrawGizmos()
         {
             GizmosUtils.DrawBounds(_allBounds, Color.magenta);
+            GizmosUtils.DrawBounds(_containerBounds, Color.yellow);
         }
     }
 }
