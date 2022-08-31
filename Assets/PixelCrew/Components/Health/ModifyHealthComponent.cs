fdf0416a94e5a995a72fe97bbbf7c500907d914e
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-08-31 19:05:33 +0300

    Applied Range Damage stat

--- Assets/PixelCrew/Components/Health/ModifyHealthComponent.cs
+++ Assets/PixelCrew/Components/Health/ModifyHealthComponent.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
+﻿using PixelCrew.Model;
+using PixelCrew.Model.Definition.Player;
+using UnityEngine;
 
 namespace PixelCrew.Components.Health
 {
     public class ModifyHealthComponent : MonoBehaviour
     {
+        [SerializeField] private bool _isHeroAttack;
+        [SerializeField] private bool _isRangeAttack;
+        
+        // ToDo hide field if it is Hero attack
         [SerializeField] private int _hpDelta;
 
         public void ApplyHealthChange(GameObject target)
         {
+            if (_isHeroAttack && _isRangeAttack)
+                UpdateHpDelta(StatId.RangeDamage);
+            
             var healthComponent = target.GetComponent<HealthComponent>();
 
             if (healthComponent == null) return;
@@ -21,7 +30,13 @@ namespace PixelCrew.Components.Health
                 var damage = -_hpDelta;
                 healthComponent.ApplyDamage(damage);
             }  
-            
+        }
+
+        private void UpdateHpDelta(StatId statId)
+        {
+            var session = FindObjectOfType<GameSession>();
+            var damage = session.StatsModel.GetValue(statId);
+            _hpDelta = - (int) damage;
         }
     }
 }
