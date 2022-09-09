86b0b269527fc58cb84fb82190aac8de5b53ef63
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-09-09 15:20:05 +0300

    Boss Fight

---
+++ Assets/PixelCrew/Creatures/Mobs/Boss/ChangeLightsComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace PixelCrew.Creatures.Mobs.Boss
+{
+    public class ChangeLightsComponent : MonoBehaviour
+    {
+        [SerializeField] private Light2D[] _lights;
+        
+        [ColorUsage(true, true)]
+        [SerializeField] private Color _color;
+
+        [ContextMenu("SetColor")]
+        public void SetColor()
+        {
+            foreach (var light in _lights)
+            {
+                light.color = _color;
+            }
+        }
+    }
+}
---
+++ Assets/PixelCrew/Creatures/Mobs/Boss/States/BossShootState.cs
@@ -0,0 +1,14 @@
+﻿using PixelCrew.Components.GoBased;
+using UnityEngine;
+
+namespace PixelCrew.Creatures.Mobs.Boss.States
+{
+    public class BossShootState : StateMachineBehaviour
+    {
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var spawner = animator.GetComponent<CircularProjectileSpawner>();
+            spawner.LaunchProjectile();
+        }
+    }
+}
