ed997757b3c8bf54049ecac5bb22cc2f7cafc2fc
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-09-17 19:41:38 +0300

    Simple Analytics

--- Assets/PixelCrew/UI/Windows/AnimatedWindow.cs
+++ Assets/PixelCrew/UI/Windows/AnimatedWindow.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Analytics;
 
 namespace PixelCrew.UI.Windows
 {
@@ -10,6 +11,7 @@ namespace PixelCrew.UI.Windows
 
         protected virtual void Start()
         {
+            AnalyticsEvent.ScreenVisit(gameObject.name);
             _animator = GetComponent<Animator>();
 
             _animator.SetTrigger(ShowKey);
