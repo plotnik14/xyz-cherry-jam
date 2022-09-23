0c07ed51f68e868891228c2188fa5eefe596966d
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-09-23 21:07:43 +0300

    Level06 is ready

---
+++ Assets/PixelCrew/Components/GameplayTrigger/GameplayTriggersGroupComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using PixelCrew.Utils.Disposables;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew.Components.GameplayTrigger
+{
+    public class GameplayTriggersGroupComponent : MonoBehaviour
+    {
+        [SerializeField] private List<GameplayTriggerComponent> _triggers;
+        [SerializeField] private UnityEvent _onAllActivated;
+
+        private readonly CompositeDisposable _disposable = new CompositeDisposable();
+        
+        private void Start()
+        {
+            foreach (var trigger in _triggers)
+            {
+                _disposable.Retain(trigger.Subscribe(OnTriggerActivated));
+            }
+        }
+
+        private void OnTriggerActivated()
+        {
+            foreach (var trigger in _triggers)
+            {
+                if (!trigger.IsActivated) return;
+            }
+            
+            _onAllActivated?.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            _disposable.Dispose();
+        }
+    }
+}
