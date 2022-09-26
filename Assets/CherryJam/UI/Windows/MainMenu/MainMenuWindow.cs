e3467d7d38b4dd9d2449ba83e6a80ea451cf7ded
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-09-27 01:01:21 +0300

    Save progress

--- Assets/CherryJam/UI/Windows/MainMenu/MainMenuWindow.cs
+++ Assets/CherryJam/UI/Windows/MainMenu/MainMenuWindow.cs
@@ -19,7 +19,7 @@ namespace CherryJam.UI.Windows.MainMenu
             _closeAction = () =>
             {
                 var loader = FindObjectOfType<LevelLoader>();
-                loader.LoadLevel("Level01_Puzzle");
+                loader.LoadLevel("Level1");
             };
             
             Close();
