799eb15656a22d8c5668530a502476fba10981f3
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-10-07 19:39:04 +0300

    Menu sounds

--- Assets/CherryJam/UI/Widgets/ButtonSound.cs
+++ Assets/CherryJam/UI/Widgets/ButtonSound.cs
@@ -4,20 +4,31 @@ using UnityEngine.EventSystems;
 
 namespace CherryJam.UI.Widgets
 {
-    public class ButtonSound : MonoBehaviour, IPointerClickHandler
+    public class ButtonSound : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
     {
-        [SerializeField] private AudioClip _audioClip;
+        [SerializeField] private AudioClip _pointerClick;
+        [SerializeField] private AudioClip _pointerEnter;
         
         private AudioSource _source;
 
         public void OnPointerClick(PointerEventData eventData)
+        {
+            PlaySound(_pointerClick);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            PlaySound(_pointerEnter);
+        }
+
+        private void PlaySound(AudioClip sound)
         {
             if (_source == null)
             {
                 _source = AudioUtils.FindSfxSource();
             }
 
-            _source.PlayOneShot(_audioClip);
+            _source.PlayOneShot(sound);
         }
     }
 }
