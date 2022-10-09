2f2a0376ce942cd0327c53c4782789ce6913a5b1
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-10-09 13:40:33 +0300

    Lookahead for camera

---
+++ Assets/CherryJam/Components/LevelManagement/FollowCameraController.cs
@@ -0,0 +1,21 @@
+﻿using CherryJam.Creatures.Hero;
+using UnityEngine;
+using Cinemachine;
+
+namespace CherryJam.Components.LevelManagement
+{
+    [RequireComponent(typeof(CinemachineVirtualCamera))]
+    public class FollowCameraController : MonoBehaviour
+    {
+        private Hero _hero;
+        private CinemachineVirtualCamera _vCamera;
+        
+        private void Start()
+        {
+            _hero = FindObjectOfType<Hero>();
+            
+            _vCamera = GetComponent<CinemachineVirtualCamera>();
+            _vCamera.Follow =_hero.transform;
+        }
+    }
+}
