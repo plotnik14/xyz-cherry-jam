6cfab6c1fe3a40c988bd245549d691a5607440a5
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-10-09 22:01:37 +0300

    Added enemy soul after death

---
+++ Assets/CherryJam/Components/Movement/MovementToAltarComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CherryJam.Components.Movement
+{
+    public class MovementToAltarComponent : MovementToTargetComponent
+    {
+        protected override void Start()
+        {
+            _destination = GameObject.FindWithTag("Altar").transform;
+            base.Start();
+        }
+    }
+}
---
+++ Assets/CherryJam/Components/Movement/MovementToTargetComponent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CherryJam.Components.Movement
+{
+    public class MovementToTargetComponent : MonoBehaviour
+    {
+        [SerializeField] protected Transform _destination;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _reachTreshold;
+        [SerializeField] private UnityEvent _OnReached;
+
+        protected virtual void Start()
+        {
+            StartMovement();
+        }
+
+        private void StartMovement()
+        {
+            StartCoroutine(Move());
+        }
+
+        private IEnumerator Move()
+        {
+            var progress = 0f;
+            
+            while (!IsDestinationReached())
+            {
+                progress += _speed;
+                transform.position = Vector3.Lerp(transform.position, _destination.position, progress);
+                yield return null;
+            }
+            
+            _OnReached?.Invoke();
+        }
+        
+        private bool IsDestinationReached()
+        {
+            return (_destination.position - transform.position).magnitude < _reachTreshold;
+        }
+    }
+}
