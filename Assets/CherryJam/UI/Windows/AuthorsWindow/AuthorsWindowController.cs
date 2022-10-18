2292d1e00b99ae4b1408fc86f8afa27baf9f73d5
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-10-18 21:29:03 +0300

    Authors page update

---
+++ Assets/CherryJam/UI/Windows/AuthorsWindow/AuthorsWindowController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CherryJam.UI.Windows.AuthorsWindow
+{
+    public class AuthorsWindowController : MonoBehaviour
+    {
+        private GameObject _canvasObj;
+        private void Awake()
+        {
+            StartCoroutine(ShowAuthorsWindow());
+        }
+
+        private IEnumerator ShowAuthorsWindow()
+        {
+            var window = Resources.Load<GameObject>("UI/AuthorsWindow");
+            
+            while (_canvasObj == null)
+            {
+                _canvasObj = GameObject.FindWithTag("MainUICanvas");
+                yield return null;
+            }
+            
+            Instantiate(window, _canvasObj.GetComponent<Canvas>().transform);
+        }
+    }
+}
