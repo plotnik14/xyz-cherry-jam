a6b84a37e07da800b4c3cdfe2a7a5a92f6db0a9a
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-10-23 19:28:58 +0300

    Added video cutscene prototype

---
+++ Assets/CherryJam/Components/CutScenes/VideoCutScene/VideoCutSceneController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.Video;
+
+namespace CherryJam.Components.CutScenes.VideoCutScene
+{
+    public class VideoCutSceneController : MonoBehaviour
+    {
+        [SerializeField] private VideoPlayer _videoPlayer;
+        [SerializeField] private UnityEvent _onVideoFinished;
+
+        private void Start()
+        {
+            _videoPlayer.loopPointReached += OnVideoEndAction;
+            _videoPlayer.Play();
+        }
+
+        private void OnVideoEndAction(VideoPlayer source)
+        {
+            _onVideoFinished?.Invoke();
+        }
+        
+        public void OnSkipCutScene(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                SkipCutScene();
+        }
+
+        private void SkipCutScene()
+        {
+            if (_videoPlayer.isPlaying)
+            {
+                _videoPlayer.Stop();
+                _onVideoFinished?.Invoke();
+            }
+        }
+    }
+}
