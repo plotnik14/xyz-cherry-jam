996af966e790ef6a62903a735db32fa11436503c
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-12-03 19:34:42 +0300

    Added shooting trap

--- Assets/CherryJam/Creatures/Mobs/ShootingTrapAI.cs
+++ Assets/CherryJam/Creatures/Mobs/ShootingTrapAI.cs
@@ -8,94 +8,55 @@ namespace CherryJam.Creatures.Mobs
 {
     public class ShootingTrapAI : MonoBehaviour
     {
-        [SerializeField] protected bool _usedByGroup;
-        [SerializeField] protected ColliderCheck _vision;
-        [SerializeField] protected bool _shootAlways;
-
-        [Header("Range")]
-        [SerializeField] protected Cooldown _rangeCooldown;
-        [SerializeField] protected SpawnComponent _rangeAttack;
-
-        protected Animator _animator;
-        protected PlaySoundsComponent Sounds;
-        protected bool IsFrozen;
-        protected readonly Cooldown FreezeCooldown = new Cooldown();
+        [SerializeField] private ColliderCheck _vision;
         
-        private Color _colorBeforeFreezing;
+        [Header("Attack")]
+        [SerializeField] private Cooldown _rangeCooldown;
+        [SerializeField] private DirectionalSpawnComponent _rangeAttack;
+        [SerializeField] private bool _shootAlways;
 
-        protected static readonly int Range = Animator.StringToHash("range");
+        private Animator _animator;
+        private PlaySoundsComponent _sounds;
+
+        private static readonly int AttackKey = Animator.StringToHash("attack");
+        
+        private Hero.Hero _hero;
 
         private void Awake()
         {
             _animator = GetComponent<Animator>();
-            Sounds = GetComponent<PlaySoundsComponent>();
+            _sounds = GetComponent<PlaySoundsComponent>();
         }
 
         private void Update()
         {
-            CheckActiveBuffs();
-            
-            if (IsFrozen) return;
-            
-            if (_usedByGroup) return;
             if (!_vision.IsTouchingLayer && !_shootAlways) return;
-            if (PerformExtraActionAndStop()) return;
 
             if (_rangeCooldown.IsReady)
-            {
                 RangeAttack();
-            }
-        }
-
-        private void CheckActiveBuffs()
-        {
-            if (IsFrozen && FreezeCooldown.IsReady)
-                Unfreeze();
-        }
-
-        public virtual bool PerformExtraActionAndStop()
-        {
-            return false;
+            
         }
 
         public void RangeAttack()
         {
-            if (!_usedByGroup)
-            {
-                _rangeCooldown.Reset();
-            }
-            
-            _animator.SetTrigger(Range);
-            Sounds.Play("Range");
+            _rangeCooldown.Reset();
+            _animator.SetTrigger(AttackKey);
+            _sounds.Play("Attack");
         }
 
-        public void OnRangeAttack()
+        public void OnAttack()
         {
-            _rangeAttack.Spawn();
-        }
-
-        public void Freeze()
-        {
-            _animator.enabled = false;
+            if (_hero == null)
+                _hero = FindObjectOfType<Hero.Hero>();
             
-            var sprite = GetComponent<SpriteRenderer>();
-            _colorBeforeFreezing = sprite.color;
-            var frozenColor = new Color(0f, 255f, 255f);
-            sprite.color = frozenColor;
-
-            FreezeCooldown.Value = 10; // ToDo Move for defs
-            FreezeCooldown.Reset();
-            IsFrozen = true;
+            var directionToHero = GetDirectionToTarget(_hero.gameObject);
+            _rangeAttack.Spawn(directionToHero);
         }
         
-        private void Unfreeze()
+        private Vector2 GetDirectionToTarget(GameObject target)
         {
-            IsFrozen = false;
-            
-            var sprite = GetComponent<SpriteRenderer>();
-            sprite.color = _colorBeforeFreezing;
-            
-            _animator.enabled = true;
+            var direction = target.transform.position - transform.position;
+            return direction.normalized;
         }
     }
 }
