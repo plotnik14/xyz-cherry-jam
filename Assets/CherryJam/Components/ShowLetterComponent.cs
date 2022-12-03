d50383a04674b779bfe1c23aabbea681883fed74
Alexander Plotnikov <plotnikov93av@gmail.com>
2022-12-03 22:56:34 +0300

    Dead student logic + Letter

---
+++ Assets/CherryJam/Components/ShowLetterComponent.cs
@@ -0,0 +1,18 @@
+﻿using CherryJam.UI.Hud.Letter;
+using UnityEngine;
+
+namespace CherryJam.Components
+{
+    public class ShowLetterComponent : MonoBehaviour
+    {
+        [SerializeField][TextArea] private string _text;
+
+        public void Show()
+        {
+            var letterBox = GameObject.FindWithTag("LetterBox");
+            var letterController = letterBox.GetComponent<LetterController>();
+
+            letterController.Show(_text);
+        }
+    }
+}
---
+++ Assets/CherryJam/UI/Hud/Letter/LetterController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CherryJam.Creatures.Hero;
+using CherryJam.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CherryJam.UI.Hud.Letter
+{
+    public class LetterController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _container;
+        [SerializeField] private Text _text;
+        
+        private Hero _hero;
+
+        public void Show(string text)
+        {
+            TimeUtils.StopTime();
+            
+            if (_hero == null)
+                _hero = FindObjectOfType<Hero>();
+            _hero.LockInput();
+            
+            _text.text = text;
+            _container.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            TimeUtils.ResumeTime();
+            
+            _container.SetActive(false);
+            _text.text = String.Empty;
+            
+            _hero.UnlockInput();
+        }
+    }
+}
